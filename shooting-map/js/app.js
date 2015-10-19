1a0b152e86781e3d80e466b70d46859acf779787
Luis Naranjo <luisnaranjo733@gmail.com>
2015-10-18 17:34:01 -0700

    leaflet/mapbox map working with custom tiles

--- shooting-map/js/app.js
+++ shooting-map/js/app.js
@@ -2,3 +2,11 @@
 
 'use strict';
 
+var map = L.map('map-container').setView([51.505, -0.09], 13);
+
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+    maxZoom: 18,
+    id: 'luisnaranjo733.no8peoka',
+    accessToken: 'pk.eyJ1IjoibHVpc25hcmFuam83MzMiLCJhIjoiY2lmeDVra3Q1M3A0Z3U2a3N3d2JzNXFicCJ9.nLZGt1FxRVUxOUL-_1wrIg'
+}).addTo(map);
