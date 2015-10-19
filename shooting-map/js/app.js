8933312f10d0eb19f90e43b4b97f80b2db66cbc7
Luis Naranjo <luisnaranjo733@gmail.com>
2015-10-19 16:57:37 -0700

    epiphany

--- shooting-map/js/app.js
+++ shooting-map/js/app.js
@@ -6,6 +6,11 @@ var overlayMaps = {
     "Unknown": L.layerGroup([]),
 };
 
+var killed_male_pct_obj = $('#killed-male');
+var killed_female_pct_obj = $('#killed-female');
+var hit_male_pct_obj = $('#hit-male');
+var hit_female_pct_obj = $('#hit-female');
+
 var stats = {
     'Male': {
         'Killed': 0,
@@ -16,24 +21,52 @@ var stats = {
         'Killed': 0,
         'Hit': 0,
         'Unknown': 0
-    }
+    },
+    'Killed': 0,
+    'Hit': 0
 }
 
+function round(value, decimals) {
+    return Number(Math.round(value+'e'+decimals)+'e-'+decimals);
+}
+
+var toPercentage = function(decimal_value) {
+    return round(decimal_value * 100, 2);
+}
+
+var updateStats = function(stats) {
+    console.log('Updating stats');
+
+    var killed_male_pct = toPercentage(stats.Male.Killed / stats.Killed);
+    killed_male_pct_obj.html(killed_male_pct);
+
+    var killed_female_pct = toPercentage(stats.Female.Killed / stats.Killed);
+    killed_female_pct_obj.html(killed_female_pct);
+
+
+    var hit_male_pct = toPercentage(stats.Male.Hit / stats.Hit);
+    hit_male_pct_obj.html(hit_male_pct);
+
+    var hit_female_pct = toPercentage(stats.Female.Hit / stats.Hit);
+    hit_female_pct_obj.html(hit_female_pct);
+}
+
+
 var parseShootings = function(data) {
-    console.log('parsing');
     $.each(data, function(i, event ) {
         var marker = L.circleMarker([event.lat, event.lng]).bindPopup(event.summary);
         marker.addTo(overlayMaps[event.outcome]);
         if (event.victim.gender != 'Unknown') {
-            console.log(event.victim.gender);
             stats[event.victim.gender][event.outcome] += 1;
+            stats[event.outcome] += 1  
         }
     });
+    console.log('DONE');
+    console.log(stats);
     
 }
 
 $.getJSON('data/data.min.json').then(parseShootings);
-console.log(stats);
 
 
 var map = L.map('map-container').setView([40.913129, -102.491385], 5);
@@ -48,5 +81,10 @@ L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={
 L.control.layers(null, overlayMaps).addTo(map);
 
 map.on('overlayadd', function(e) {
-    // 
+    updateStats(stats);
 });
+
+map.on('overlayremove', function(e) {
+    updateStats(stats);
+});
+
