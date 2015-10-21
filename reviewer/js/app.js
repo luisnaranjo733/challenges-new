7a767be1186a791078f41d800a8b196c5873d103
Luis Naranjo <luisnaranjo733@gmail.com>
2015-10-21 00:04:57 -0700

    Form is being intercepted beautifully!

--- reviewer/js/app.js
+++ reviewer/js/app.js
@@ -1,7 +1,23 @@
 $(function() {
-    var avg_movie_rating = 3;
-    $('#movie_avg_raty').raty({
-        'score': avg_movie_rating,
-        'readOnly': true
+    $("#review-submission-form").submit(function(event){
+        var form = {
+            'rating_stars': $('#submission-raty').raty('score'),
+            'rating_title': $('#reviewTitle').val(),
+            'rating_body': $('#reviewBody').val(),
+        };
+        console.log(form);
+
+        event.preventDefault();    //current standard
+        event.returnValue = false; //some older browsers
+        return false;              //most older browsers
     });
+    // Display average rating for movie
+    // var avg_raty = 3;
+    // $('#avg-raty').raty({
+    //     'score': avg_raty,
+    //     'readOnly': true
+    // });
+
+    $('#submission-raty').raty();
 });
+
