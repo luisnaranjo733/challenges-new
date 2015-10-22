f524563080b69e1b0f060a0a339b10e4faa4d801
Luis Naranjo <luisnaranjo733@gmail.com>
2015-10-22 14:51:51 -0700

    Added notes for remaining requirements

--- reviewer/js/app.js
+++ reviewer/js/app.js
@@ -17,8 +17,6 @@ function deleteReview(close_icon) {
             item.destroy({});
         });
     }).then(updateAvgReview);
-    console.log('deleting review');
-    
 
     var reviewDiv = $('#saved-review-' +  objectID);
     reviewDiv.remove();
@@ -59,7 +57,6 @@ function addReviewToDOM(data, created) {
 }
 
 function updateAvgReview() {
-    console.log('Updating avg review');
     var query = new Parse.Query(Review);
 
     query.find().then(function(results) {
@@ -72,7 +69,7 @@ function updateAvgReview() {
         });
         var avg_rating = ratings_sum / ratings_count;
         avg_rating = round(avg_rating, 1);
-        console.log('avg: ' +  avg_rating);
+        console.log('Upaged avg: ' +  avg_rating);
 
         // Display average rating for movie at movie description header
         $('#avg-raty').raty({
@@ -145,6 +142,7 @@ $(function() {
         $('#reviewBody').val('')
         $('#submission-raty').raty('score', 0)
 
+ 
         var data = {
             'title': form.title,
             'content': form.content,
@@ -160,3 +158,49 @@ $(function() {
     });
 });
 
+/* Remaining requirements
+
+* Because the review title and body are entered by the user and integrated into the page
+    by your code, you must protect against script injection attacks. The easiest way to
+    do this is to use jQuery's text() method instead of it's html() method when
+    populating the review title and body elements. The text() method escapes the HTML,
+    so any embedded HTML will just show up literally on the page, and will not be
+    interpreted as HTML by the browser.
+    - tl;dr prevent script injection attacks
+
+* Users must be able to indicate whether a particular review was helpful or unhelpful
+    (e.g., up/down vote icons or yes/no buttons), and these vote totals must be
+    persisted with the review object on Parse. Note that multiple users may be voting
+    on the same review at the same time, so you should use Parse's atomic increment
+    function to safely increment the values. To keep things simple, you may let users
+    vote on their own reviews and vote multiple times. Check out the advanced
+    extra-credit requirements if you want to enable authentication and authorization
+    rules that prohibit this.
+    - tl;dr atomic review upvote/downvote
+
+* You must also display the vote totals with the review. How you display these vote
+    totals is up to you, but look at how Amazon, Yelp, and other review sites do it.
+    A typical approach is to build a string that says something like "2 out of 5
+    people found this review helpful," but you may opt to show a percentage (texturally
+    or graphically) instead.
+    - tl;dr Display review upvote/downvote
+
+* Because communication with the Parse server is asynchronous and not instantaneous,
+    you must display visual feedback to the user that a save or fetch operation is in
+    progress. This is typically done using some kind of animated icon that is shown while
+    the request is happening, and hidden when the request ends. An easy and attractive
+    option is to use the spin feature of the Font Awesome library.
+    - tl;dr Visual feedback for the user during fetch operations
+
+* If any of the Parse operations return an error, you should display the error's message
+property on the page so the user knows why the operation failed. If you are using Bootstrap,
+remember that they define style classes for alerts that are appropriate for displaying
+errors and other feedback to the user. You can even make these alerts interactive.
+    - tl;dr Display error messages to user from parse operations
+
+* Testing with a friend
+* Extra Credit
+    - Authentication/Authorization
+    - Multiple things to review
+    - Use a templating engine
+*/
