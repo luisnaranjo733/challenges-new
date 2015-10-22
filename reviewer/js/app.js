bb899921c91abce43bb62c9225885af8649606b1
Luis Naranjo <luisnaranjo733@gmail.com>
2015-10-21 22:34:19 -0700

    js html creation is working

--- reviewer/js/app.js
+++ reviewer/js/app.js
@@ -2,15 +2,51 @@ Parse.initialize("lnI74xBuUcxJ5JwlWcdRMNfMlBSvvPRh5v2WRWVU", "iWPTeMpRzvObmJk2nd
 var Review = Parse.Object.extend('Review');
 var raty_id = 0
 
-function loadUser(params) {
-  $.get('load_review.mst', function(template) {
-    var rendered = Mustache.render(template, params);
-    $('#saved-reviews').append(rendered);
-    $('#review-raty-' + params['raty-id']).raty({
-        'readOnly': true,
-        'score': params['rating'],
+var deleteReview = function(e) {
+    console.log(e);
+}
+
+// data properties
+// title, content, raty-id, rating
+/*
+<div class="saved-review">
+    <h3>{{ title }}</h3>
+    <i id="review-close-{{ raty-id }}" class="fa fa-close exit-icon"></i>
+    <i class="fa fa-thumbs-down thumbs-icon"></i>
+    <i class="fa fa-thumbs-up thumbs-icon"></i>
+
+    <div id="review-raty-{{ raty-id }}" class="raty"></div>
+    <p>{{ body }}</p>
+</div>
+
+Append to #saved-reviews
+
+$('<div>')                      // Creates the element
+    .attr('spry:region','myDs') // Sets the attribute spry:region="myDs"
+    .html('{hostname}')         // Sets the inner HTML to {hostname}
+    .appendTo('body')    
+
+        $([h3[0], close_icon[0]]).appendTo(review_div);
+*/
+function addReviewToHTML(data) {
+    var review_div = $('<div>').attr('class', 'saved-review');
+    var h3 = $('<h3>').text(data['title']);
+    var close_icon = $('<i>').attr('id', 'review-close-' +  data['raty-id']);
+    close_icon.attr('class', 'fa fa-close exit-icon');
+    //close_icon.click();
+    var thumbs_up_icon = $('<i>').attr('class', 'fa fa-thumbs-up thumbs-icon');
+    var thumbs_down_icon = $('<i>').attr('class', 'fa fa-thumbs-down thumbs-icon');
+    var raty_div = $('<div>').attr('id', 'review-raty-' + data['raty-id']);
+    raty_div.attr('class', 'raty');
+    var body = $('<p>').text(data.content);
+
+    var children = [h3, close_icon, thumbs_up_icon, thumbs_down_icon, raty_div, body];
+    children.forEach(function(element, i) {
+        children[i] = element.get(0);
     });
-  });
+    $(children).appendTo(review_div);
+    review_div.appendTo($('#saved-reviews'));
+    
 }
 
 $(function() {
@@ -31,13 +67,13 @@ $(function() {
         results.forEach(function(item) {
             var rating = item.get('rating');
             raty_id += 1;
-            var params = {
+            var data = {
                 'title': item.get('title'),
                 'content': item.get('content'),
                 'raty-id': raty_id,
                 'rating': rating
             }
-            loadUser(params);
+            addReviewToHTML(data);
         });
     });
 
