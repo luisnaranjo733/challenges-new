cb76e9b8419ea86ceb53068f5df55bd3fdb22e6f
Luis Naranjo <luisnaranjo733@gmail.com>
2015-10-27 23:23:42 -0700

    like lookup from username is working!

--- soundcloud/js/app.js
+++ soundcloud/js/app.js
@@ -1,17 +1,66 @@
 'use strict';
 
 var BASE_URL = 'https://api.soundcloud.com'; //website we fetch information from
-var CLIENT_ID = 'd2de86b6f2a8c564b00e1f78421fab9d' //application ID for requests
+var CLIENT_ID = '1ad29a5353733827b97f335387269fa1' //application ID for requests
+
+// user id = 5678925
+
+var url_wrapper = function(url) {
+    return BASE_URL + url +'client_id='+ CLIENT_ID;
+}
+
+
+// http://api.soundcloud.com/users/5678925?client_id=1ad29a5353733827b97f335387269fa1
+var getUserURL = function(user_id) {
+    return BASE_URL + '/users/' +  user_id + '?client_id='+ CLIENT_ID;
+}
+
+// https://api.soundcloud.com/users/5678925/favorites?client_id=1ad29a5353733827b97f335387269fa1
+var getUserFavoritesURL = function(user_id) {
+    return BASE_URL + '/users/' +  user_id + '/favorites?client_id='+ CLIENT_ID;
+}
+
+// https://api.soundcloud.com/resolve?url=https://soundcloud.com/doubledubba&client_id=1ad29a5353733827b97f335387269fa1
+var getUserIdURL = function(username) {
+    var url = 'https://soundcloud.com/' + username;
+    return BASE_URL + '/resolve?url=' + url + '&client_id='+ CLIENT_ID;
+    // response.data.id
+}
 
 var myApp = angular.module('myApp', [])
-  .controller('MyCtrl', ['$scope', '$http', function($scope, $http) { 
+    .controller('MyCtrl', ['$scope', '$http', function($scope, $http) { 
+    $scope.query = 'doubledubba';
   
     //function called to fetch tracks based on the scope's query
     $scope.getTracks = function() {
-      var request = BASE_URL + '/tracks' + '?' +'client_id='+ CLIENT_ID + '&q=' + $scope.query; //build the RESTful request
-      $http.get(request) //Angular AJAX call
+        var username = $scope.query;
+        var request = getUserIdURL(username); //build the RESTful request UR
+        $http.get(request) //Angular AJAX call
         .then(function (response) {
-          $scope.tracks = response.data; //save results to available model
+            var userId = response.data.id;
+            request = getUserFavoritesURL(userId);
+            $http.get(request).then(function(response) {
+                console.log(response);
+                $scope.tracks = response.data; //save results to available model
+            })
+            
         });
     };
 }])
+
+/* Assignment requirements 
+
+* Use the RESTful API to get data
+  - Use at least one of the API endpoints
+* Use Angular directives to display data
+  - Data retrieved from API should be directly linked to the view
+  - No submit button on form
+* Include an HTML Form with Angular and Validation
+  -Application should impose and validate some requirements on the form.
+  or example, maybe there is a minimum number of characters needed to
+  search for a song, or the user must fill out all options before searching.
+  Be sure and give the user feedback regarding their entry. 
+
+* Do something creative
+
+*/
