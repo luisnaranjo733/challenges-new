ad5845f629886f2c41ee7f79ccc8d3be7eb6ae72
Luis Naranjo <luisnaranjo733@gmail.com>
2015-11-07 12:58:22 -0800

    order detail framework is working

--- dawg-coffee/js/app.js
+++ dawg-coffee/js/app.js
@@ -15,8 +15,14 @@ app.config(function($stateProvider, $urlRouterProvider) {
        templateUrl: 'partials/order.html',
        controller: 'OrderCtrl',
     });
-    $urlRouterProvider.otherwise('/');
 
+    $stateProvider.state('OrderDetail', {
+       url: '/orders/{id}', 
+       templateUrl: 'partials/order_detail.html',
+       controller: 'OrderDetailCtrl',
+    });
+
+    //$urlRouterProvider.otherwise('/');
 })
 
 app.controller('HomeCtrl', ['$scope', function($scope) {
@@ -26,7 +32,18 @@ app.controller('HomeCtrl', ['$scope', function($scope) {
 app.controller('OrderCtrl', ['$scope', '$http', function($scope, $http) {
     $scope.hello = 'hello';
     $http.get('data/products.json').then(function(response) {
-        console.log(response.data);
         $scope.orders = response.data;
+    });
+    $scope.click = function(order) {
+        //console.log(order);
+    }
+}]);
+
+app.controller('OrderDetailCtrl', ['$scope', '$http', '$stateParams', '$filter', function($scope, $http, $stateParams, $filter) {
+    $http.get('data/products.json').then(function(response) {
+        var order = $filter('filter')(response.data, {
+            id: $stateParams.id
+        }, true)[0]
+        $scope.order = order;
     });
 }]);
