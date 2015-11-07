eb347d24a9957ba2caf6ddfb287bc3a0b587f810
Luis Naranjo <luisnaranjo733@gmail.com>
2015-11-06 23:00:44 -0800

    angular is set up
    need to set up ui router and partials

---
+++ dawg-coffee/js/app.js
@@ -0,0 +1,9 @@
+'use strict';
+
+
+var madLibApp = angular.module('CoffeeApp', [])
+.controller('HomeCtrl', ['$scope', '$http', function($scope, $http) {
+
+    $scope.message = "hello world";
+
+}])
