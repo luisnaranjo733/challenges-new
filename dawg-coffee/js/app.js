75524de1efefd4877957bb757458a2a81bf38886
Luis Naranjo <luisnaranjo733@gmail.com>
2015-11-07 00:45:32 -0800

    basic ui view is working!

--- dawg-coffee/js/app.js
+++ dawg-coffee/js/app.js
@@ -1,9 +1,19 @@
 'use strict';
 
 
-var madLibApp = angular.module('CoffeeApp', [])
-.controller('HomeCtrl', ['$scope', '$http', function($scope, $http) {
+var app = angular.module('CoffeeApp', ['ui.router']);
+
+app.config(function($stateProvider) {
+    //first param is a name (label) for the state
+    //second param is an object of options
+    $stateProvider.state('home', { // '/' (root) is also for home
+       url: '/', //the route for this state
+       templateUrl: 'partials/home.html', //path to partial to load
+    })
+})
+
+app.controller('HomeCtrl', ['$scope', '$http', function($scope, $http) {
 
     $scope.message = "hello world@";
 
-}])
+}]);
